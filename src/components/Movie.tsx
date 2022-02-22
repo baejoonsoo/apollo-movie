cba911bc08628823f634a0737cf55daa78f70de3
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-22 23:59:40 +0900

    movie style

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,15 +1,36 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 interface movieId {
   movieID: number;
+  background: string;
 }
 
-const Movie = ({ movieID }: movieId) => {
+const Movie = ({ movieID, background }: movieId) => {
   return (
-    <div>
-      <Link to={`/${movieID}`}>{movieID}</Link>
-    </div>
+    <Container>
+      <Link to={`/${movieID}`}>
+        <Poster bg={background} />
+      </Link>
+    </Container>
   );
 };
 
 export default Movie;
+
+const Container = styled.div`
+  height: auto;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+const Poster = styled.div`
+  background-image: url(${({ bg }: { bg: string }) => bg});
+  height: 0%;
+  width: 100%;
+  padding-bottom: 150%;
+  background-size: cover;
+  background-position: center center;
+`;
