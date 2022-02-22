2c3c1f95ae3f9171d53def25d0fe9359561d32ab
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-22 15:52:08 +0900

    ApolloClient

---
+++ src/apollo.ts
@@ -0,0 +1,12 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
+export default client;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,41 @@
+import { gql, useQuery, DocumentNode, ApolloError } from '@apollo/client';
+
+const GET_MOVIES: DocumentNode = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+interface dataType {
+  movies: {
+    __typename: string;
+    id: number;
+    medium_cover_image: string;
+  }[];
+}
+
+interface queryLoding {
+  loading: boolean;
+  error?: ApolloError | undefined;
+  data: dataType | undefined;
+}
 const Home = () => {
-  return <h1>home</h1>;
+  const { loading, data }: queryLoding = useQuery(GET_MOVIES);
+
+  if (loading) {
+    return <h1>loading...</h1>;
+  }
+
+  return (
+    <div>
+      {data?.movies.map((movie) => (
+        <h1 key={movie.id}>{movie.id}</h1>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
