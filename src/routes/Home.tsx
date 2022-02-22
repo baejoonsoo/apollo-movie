080c64a7d47d85d86b409a3f4d37798043a8613d
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-22 16:22:47 +0900

    home style

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery, DocumentNode, ApolloError } from '@apollo/client';
+import styled from 'styled-components';
 
 const GET_MOVIES: DocumentNode = gql`
   {
@@ -23,19 +24,52 @@ interface queryLoding {
   data: dataType | undefined;
 }
 const Home = () => {
-  const { loading, data }: queryLoding = useQuery(GET_MOVIES);
-
-  if (loading) {
-    return <h1>loading...</h1>;
-  }
+  const { loading }: queryLoding = useQuery(GET_MOVIES);
 
   return (
-    <div>
-      {data?.movies.map((movie) => (
-        <h1 key={movie.id}>{movie.id}</h1>
-      ))}
-    </div>
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+    </Container>
   );
 };
 
 export default Home;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
