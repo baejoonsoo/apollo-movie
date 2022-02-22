8ebdf3aebde6359c76bc533e5307068d4644784d
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-22 23:59:57 +0900

    detail style

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,5 +1,7 @@
 import { gql, useQuery } from '@apollo/client';
+import { type } from 'os';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -8,23 +10,23 @@ const GET_MOVIE = gql`
       medium_cover_image
       description_intro
       rating
+      language
     }
   }
 `;
 
 interface dataType {
-  movie: {
-    __typename: string;
-    title: String;
-    medium_cover_image: string;
-    rating: Number;
-    description_intro: String;
-  };
+  __typename?: string | undefined;
+  title: String | undefined;
+  medium_cover_image: string | undefined;
+  rating: Number | undefined;
+  description_intro: String | undefined;
+  language: string | undefined;
 }
 
 interface queryLoding {
   loading: boolean;
-  data: dataType | undefined;
+  data: { movie: dataType } | undefined;
 }
 
 const Detail = () => {
@@ -37,11 +39,42 @@ const Detail = () => {
   console.log(data);
 
   return (
-    <div>
-      {loading && <h1>loading...</h1>}
-      {!loading && data?.movie && <h1>{data.movie.title}</h1>}
-    </div>
+    <Container>
+      <Column>
+        <Title>{data?.movie.title}</Title>
+        <Subtitle>
+          {data?.movie.language} • {data?.movie.rating}
+        </Subtitle>
+        <Description>{data?.movie.description_intro}</Description>
+      </Column>
+      {loading && <Poster></Poster>}
+      {/* {!loading && data?.movie && <h1>{data.movie.title}</h1>} */}
+    </Container>
   );
 };
 
 export default Detail;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div``;
+
+const Poster = styled.div``;
+
+const Title = styled.h1`
+  font-size: 60px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 45px;
+`;
+
+const Description = styled.p``;
